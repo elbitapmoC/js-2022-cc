2fb56d683eff29b3437a2f48497d8f387fe69ce4
Ahkal <bp@sojourn.llc>
2022-03-28 14:26:29 -0400

    hoisting examples w/ comments

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-let x = "5";
-let y = 2;
-let z = 3;
-console.log(x + y + z); // IF you put a number in "" (x), the rest of the numbers will be treated as strings as well.
+x = 5 // Assign 5 to x
 
-// $ and _ are treated as letters.
+elem = document.getElementById("demo");
+elem.innerHTML = x;
+
+var x; // declare x
+
+/* 
+  WEIRD right?
+  We used the varible BEFORE defining it...
+  
+  This is hoisting.
+  JS's default behavior to move all declarations to the top of the current scope.
+*/
+
+var x; // declare x
+x = 5 // Assign 5 to x
+// Above is an example of what JS will do by default, it moves the declaration above the initialization.
+
+
+carName = "Volvo";
+let carName;
+// Can't access 'carName' before initialization. 
+// Line 22, it's declared but not initialized.
+// The block is aware of carName but can not be used intil it's defined.
+
+// JS Hoists declarations NOT initializations
+// Declare example
+var x;
+x = 5;
+
+// Initialize example
+var y = 5;
+
+// We can avoid hoisting issues by declaring variables at the top of the page and/or of the function.
