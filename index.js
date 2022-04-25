1c0e3b689bb3f9fca8cdcf5535021e3ebdc80b8b
Ahkal <bp@sojourn.llc>
2022-04-25 14:47:25 -0400

    MOre practice w/ string methods

--- index.js
+++ index.js
@@ -5,39 +5,49 @@
 // endsWith
 // search - cannot take a second argument (for start position)
 
-let str = "I'm of looking for the owner of this one of a kind glass slipper. Of, Have you seen her?"
-let a = str.indexOf('glass'); // 51
-a = str.slice(51);
+let str = "of... I'm uh... looking for the owner of this one of a kind glass slipper. of... Uh... Have you seen her?"
+let a = str.indexOf('glass'); // 54
+a = str.slice(54);
 
 let newStr = str.replace(/of/gi, 'offer');
 a = newStr.lastIndexOf('offer'); // searches backward
-a = newStr.slice(75)
+a = newStr.slice(47)
 
-a = str.indexOf('of', 30) // second param is the start position. //41
+a = str.indexOf('of', 40) // second param is the start position. //49
+a = str.slice(49);
 
-// MATCH---
-// match - searches a string and returns the matches in an aray.
+// // MATCH---
+// // match - searches a string and returns the matches in an aray.
 a = str.match(/of/ig) // [ "of", "of", "of", "Of" ]
 
-// INCLUDES---
-// includes - checks if a string contains a specific value
+// // INCLUDES---
+// // includes - checks if a string contains a specific value
 a = str.includes('owner'); // true
+// a = str.includes('!');
 
-// STARTS WITH---
-// startsWith - checks if a string starts w/ a specific value
-a = str.startsWith('I'); // true
-a = str.startsWith('i'); // false
+// // STARTS WITH---
+// // startsWith - checks if a string starts w/ a specific value
+// a = str.startsWith('I'); // true
+// a = str.startsWith('i'); // falses
 
-// ENDS WITH---
-// endsWith - checks if a string starts w/ a specific value
-a = str.endsWith('?'); // true
-a = str.endsWith('.'); // false
+// // ENDS WITH---
+// // endsWith - checks if a string starts w/ a specific value
+// a = str.endsWith('?'); // true
+// a = str.endsWith('.'); // false
 
-function reverseString(str) {
-  let a = str.split('').reverse();
-  let b = a.join('');
-  console.log(b);
+function reverseString(str){
+  return str.split("").reverse().join("");
 }
 
-reverseString(str);
+// function reverseString(str) {
+//   let a = str.split('').reverse();
+//   let b = a.join('');
+//   console.log(b);
+// }
+
+str = 'Normal String';
+console.log(`
+before: ${str}
+after: ${reverseString(str)}
+`);
 // console.log(a);
