3b11d7e621bf66768b7fb8941b0488cab403d920
Ahkal <bp@sojourn.llc>
2022-06-14 13:39:40 -0400

    adds sets and map

--- index.js
+++ index.js
@@ -6,5 +6,23 @@ const person = {
     return `${this.fN} ban ${this.heritageName}` //'this' refers to the person object.
   }
 }
+// console.log(person.fullName());
 
-console.log(person.fullName());
+// Map
+const map = new Map();
+map.set(123, 'Helooooooo');
+map.set(456, 'Nurse');
+
+// console.log(map.size);
+// console.log(map.get(123));
+
+// Set - ONLY has unique values.
+// No Key-Value pairs.
+const set = new Set();
+set.add('No');
+set.add('reps')
+set.add('here')
+console.log(set.has('no')); // false
+console.log(set.delete('No'));
+console.log(set.size); // 2
+console.log(set);
