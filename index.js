da3701141f80946eb54e2ef6478a932fcd9ca13b
Ahkal <bp@sojourn.llc>
2022-06-15 09:44:41 -0400

    errors to push, ways of logging to console

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-const person = {
-  fN: 'Ahkal',
-  heritageName: 'Yahawadah',
-  parent: true,
-  fullName : function() {
-    return `${this.fN} ban ${this.heritageName}` //'this' refers to the person object.
-  }
+try {
+  throw new Error('Hit a tree!');
+} catch (err) {
+  console.log(err);
 }
-// console.log(person.fullName());
 
-// Map
-const map = new Map();
-map.set(123, 'Helooooooo');
-map.set(456, 'Nurse');
+console.log('Value');
+console.error('Error message');
+console.debug('Debug Message');
+console.table([[1,3], ['hello' ,'world']]);
 
-// console.log(map.size);
-// console.log(map.get(123));
+console.count('key');
+console.count();
+console.countReset('key');
+console.count('key');
 
-// Set - ONLY has unique values.
-// No Key-Value pairs.
-const set = new Set();
-set.add('No');
-set.add('reps')
-set.add('here')
-console.log(set.has('no')); // false
-console.log(set.delete('No'));
-console.log(set.size); // 2
-console.log(set);
+console.time('key');
+for (let index = 0; index <100; index++) {
+  console.log(index);
+}
+// console.timeEnd('key');
+
+console.trace();
