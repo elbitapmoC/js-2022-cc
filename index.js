783cac2f3df1ba71df894aa267f0f72745123638
Ahkal <bp@sojourn.llc>
2022-06-15 10:09:10 -0400

    adds block scope and function scope example

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 let x = "5";
 let y = 2;
 let z = 3;
-console.log(x + y + z); // IF you put a number in "" (x), the rest of the numbers will be treated as strings as well.
+// console.log(x + y + z); // IF you put a number in "" (x), the rest of the numbers will be treated as strings as well.
 
 // $ and _ are treated as letters.
+
+// scope
+function test(){
+
+  if(true){
+    var test1 = 'VAR TEST 1';
+    let test2 = 'LET TEST 2';
+  }
+
+  console.log(test1);
+  console.log(test2);
+}
+
+// test();
+const cars = ['a','b','c'];
+console.log(typeof cars);
